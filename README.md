34bffefa2f131d153ad8f500ee30c833063963c5
Noxware <fprofeti98@gmail.com>
2020-12-11 19:51:57 -0300

    Added a README and small fixes in strings.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Scripts
+
+This repo contains very very small utility scripts that sometimes I use.
+
+## adb-wifi.py
+
+Guided configuration of adb over wifi. Useful for debugging mobile apps in a real device without an usb cable. (a cable is still necessary for the initial setup).
+
+## ext-fix.py
+
+Check the mime type of all files in the current directory. If those files have a wrong (or missing) file extension, this script can add the correct extension to the file name. (The original extension is preserved as part of the filename).
--- ext-fix.py
+++ ext-fix.py
@@ -42,7 +42,7 @@ def main():
                 print(f"'{p}' should have a '{real_ext}' extension.")
                 file_real_ext_map[p] = real_ext
 
-    print("\nThis script will append the correct file extension to each file. The older extension will be preserved as part of the file's name. Are you ok with this?")
+    print("\nThis script will add the correct file extension to each file. The older extension will be preserved as part of the filename. Are you ok with this?")
     
     if (ask_confirmation()):
         for p, ext in file_real_ext_map.items():
